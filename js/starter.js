1e2e4d0d1c24a9b7f488b2c64550e7ed3b6f056f
khalil <khalilelqaddabi9@gmail.com>
2025-11-06 19:59:29 +0100

    mod_progressBar

--- js/starter.js
+++ js/starter.js
@@ -1,36 +1,57 @@
-const progress = document.querySelector('.progress')
-const steps = document.querySelectorAll('.step')
-const prevbtn = document.querySelector('.prev')
-const nextbtn = document.querySelector('.next')
+const progress = document.querySelector(".progress");
+const steps = document.querySelectorAll(".step");
+const prevbtn = document.querySelector(".prev");
+const nextbtn = document.querySelector(".next");
 
-let step =0
+let step = 0;
+let totalSteps = 5
+let currentStep =1
 
-function updateProgressBar(){
-    const multiple = 100/(steps.length-1)
-    const width = step*multiple
-progress.style.transform = `scaleX(${width/100})`
+function updateProgressBar() {
+ progress.style.width = `${step}%`
+ if(currentStep===1)progress.style.width ='0%'
+ if(currentStep===2)progress.style.width ='20%'
+ if(currentStep===3)progress.style.width ='40%'
+ if(currentStep===4)progress.style.width ='60%'
+ if(currentStep===5)progress.style.width ='80%'
 }
 
 function updateStepDisplay() {
-    steps.forEach((s, index)=> {
-    s.classList.toggle('bg-orange-500', index <= step)
-    s.classList.toggle('text-white', index <= step)
-
-    })
-}
-
-function updateButtonState(){
-  prevbtn.disabled = step === 0
-nextbtn.disabled = step === steps.length - 1
-
-}
-
-let interval =setInterval(() =>{
-    updateProgressBar()
-    updateStepDisplay()
-    updateButtonState()
-    step++
-    if(step >= steps.length){
-        clearInterval(interval)
+  steps.forEach( ( s, index) =>{
+    if(index + 1 === currentStep) {
+      s.classList.add("active-icon")
+       console.log("step", currentStep, "index", index);
     }
-},2000)
+  })
+}
+updateStepDisplay()
+
+function showSections(){
+  currentStep ==1
+  
+}
+showSections()
+
+nextbtn.addEventListener("click",() =>{
+  if(currentStep < totalSteps){
+    currentStep++
+    updateProgressBar()
+    showSections()
+    updateStepDisplay()
+  }
+})
+
+prevbtn.addEventListener("click", () => {
+  if(currentStep>1){
+    currentStep--
+    updateProgressBar()
+    showSections()
+  }
+})
+
+
+
+
+
+
+
