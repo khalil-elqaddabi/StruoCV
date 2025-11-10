c33a33d5b78524baa6ee7228cd8d549159ff30c7
khalil <khalilelqaddabi9@gmail.com>
2025-11-10 21:02:33 +0100

    feat(index&starter): creat Cursus universitaire form with add more than 1

--- js/starter.js
+++ js/starter.js
@@ -1,3 +1,4 @@
+
 const progress = document.querySelector(".progress");
 const steps = document.querySelectorAll(".step");
 const prevbtn = document.querySelector(".prev");
@@ -87,13 +88,152 @@ prevbtn.addEventListener("click", () => {
 })
 
 
-  const quill = new Quill('#editor1', {
-  modules: { toolbar: true },
-  theme: 'snow'
+const quill = new Quill('#editor-container', {
+  theme: 'snow',
+  placeholder: 'Décrivez-vous ici...',
+  modules: {
+    toolbar: [
+      ['bold', 'italic', 'underline'],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      [{ 'align': [] }],
+      ['clean']
+    ]
+  }
 });
+// compitance
+function addCompitance(){
+  const ul=document.getElementById('compit')
+  const condidat=document.getElementById('condidat')
+  if(condidat.value.trim() === "") return;
+  let li =document.createElement('li')
+  li.classList.add('listC')
+  
+  li.innerHTML = `
+    <span>${condidat.value}</span>
+    <button class="removeBtn text-red-500 font-bold ml-3">X</button>
+  `;
+
+  // delete button
+  li.querySelector(".removeBtn").addEventListener("click", () => {
+    li.remove();
+  });
+  ul.appendChild(li)
+  condidat.value = "";
+}
+
+// Soft skills
+function addSoft(){
+  const ul=document.getElementById('soft')
+  const soft=document.getElementById('isoft')
+  if(soft.value.trim() === "") return;
+  let li =document.createElement('li')
+  li.classList.add('listC')
+  
+  li.innerHTML = `
+    <span>${soft.value}</span>
+    <button class="removeBtn text-red-500 font-bold ml-3">X</button>
+  `;
+
+  // delete button
+  li.querySelector(".removeBtn").addEventListener("click", () => {
+    li.remove();
+  });
+  ul.appendChild(li)
+  soft.value = "";
+}
+
+// loisir
+function addLoi(){
+  const ul=document.getElementById('loi')
+  const soft=document.getElementById('iloi')
+  if(soft.value.trim() === "") return;
+  let li =document.createElement('li')
+  li.classList.add('listC')
+  
+  li.innerHTML = `
+    <span>${soft.value}</span>
+    <button class="removeBtn text-red-500 font-bold ml-3">X</button>
+  `;
+
+  // delete button
+  li.querySelector(".removeBtn").addEventListener("click", () => {
+    li.remove();
+  });
+  ul.appendChild(li)
+  soft.value = "";
+}
 
 
 
+// addlangue
+function addLangue(){
+  const ul=document.getElementById('langue')
+  const ilangue=document.getElementById('ilangue')
+  const choi=document.getElementById('choi')
+  if(ilangue.value.trim() === "") return;
+  let li =document.createElement('li')
+  li.classList.add('listC')
+
+  
+  
+  li.innerHTML = `
+  <div>
+    <div> <i>languem</i> : ${ilangue.value}</div>
+    <div> <i>niveau</i> : ${choi.value}</div>
+    </div>
+    <button class="removeBtn text-red-500 font-bold ml-3">X</button>
+  `;
+
+  // delete button
+  li.querySelector(".removeBtn").addEventListener("click", () => {
+    li.remove();
+  });
+  ul.appendChild(li)
+  ilangue.value = "";
+}
+
+
+
+// adduniversity
+function addUni(){
+  const ul=document.getElementById('uni')
+  const iuni=document.getElementById('iuni')
+  const info=document.getElementById('iinfo')
+  const inst=document.getElementById('inst')
+  const loc=document.getElementById('loc')
+  const ds=document.getElementById('ds')
+  const de=document.getElementById('de')
+  if(iuni.value.trim() === "" || info.value.trim()==='') return;
+  let li =document.createElement('li')
+  li.classList.add('listC')
+
+  
+  
+  li.innerHTML = `
+  <div>
+    <div> <span class="text-gray-500">Univercity</span> : ${iuni.value}</div>
+    <div> <span class="text-gray-500">domine</span> : ${info.value}</div>
+    <div> <span class="text-gray-500">institution</span> : ${inst.value}</div>
+    <div> <span class="text-gray-500">location</span> : ${loc.value}</div>
+    <div> <span class="text-gray-500>startDate</span> : ${ds.value}</div>
+    <div> <span class="text-gray-500>endDate</span> : ${de.value}</div>
+    </div>
+    <button class="removeBtn text-red-500 font-bold ml-3">X</button>
+  `;
+
+  // delete button
+  li.querySelector(".removeBtn").addEventListener("click", () => {
+    li.remove();
+  });
+  ul.appendChild(li)
+  iuni.value = "";
+  info.value = "";
+  inst.value = "";
+  loc.value = "";
+  ds.value = "";
+  de.value = "";
+  
+}
 
 
 
